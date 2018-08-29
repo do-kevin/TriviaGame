52fa267152bddd03975ef497bb9519c0ffb78289
Kevin Do <do.kevin.vo@gmail.com>
2018-08-29 01:12:26 -0700

    v0.2: Logic w/ IFs need to be adjusted; may need to use for loops

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,35 +2,23 @@ alert("JavaScript is linked");
 
 
 //Variables
-var number = 100;
+var number = 5;
 var intervalId;
 
 var correctAnswers = 0;
 var incorrectAnswers = 0;
 var unAnswers = 0;
 
+var radioValue;
+
 // Functions
 
 // var radioValue = $("input[name='choice']").val();
 
 
-$("input[type='radio']").click(function(){
-    var radioValue = $("input[name='choice']:checked").val();
-    // console.log(radioValue);
-    if(radioValue === "wrongChoice"){
-        incorrectAnswers++;
-        
-        console.log("User submitted: " + radioValue);
-        $("#incorrectAnswers").html(incorrectAnswers);
-    }
-
-    else if(radioValue === "rightChoice"){
-        correctAnswers++;
-        
-        console.log("User submitted: " + radioValue);
-        $("#correctAnswers").html(correctAnswers);
-        
-    }
+$("input[type='radio']").click(function() {
+    radioValue = $("input[name=choice]:checked").val();
+    console.log(radioValue);
 });
 
 
@@ -56,6 +44,20 @@ function timerDown() {
 
     if(number === 0) {
         stopTimer();
+        if(radioValue === "wrongChoice"){
+            incorrectAnswers++;
+            
+            console.log("User submitted: " + radioValue);
+            $("#incorrectAnswers").html(incorrectAnswers);
+        }
+    
+        else if(radioValue === "rightChoice"){
+            correctAnswers++;
+            
+            console.log("User submitted: " + radioValue);
+            $("#correctAnswers").html(correctAnswers);
+            
+        }
     }
 }
 
