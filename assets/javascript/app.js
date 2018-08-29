8b79a4bdc6cfa62f03560ddd04581b97bee0d4dd
Kevin Do <do.kevin.vo@gmail.com>
2018-08-29 02:04:07 -0700

    v0.3: Deployable

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,31 +1,27 @@
-alert("JavaScript is linked");
-
-
-//Variables
-var number = 5;
+//+-+-+-+-+-+-+-+-+-+-+-+-+-+- Global Variables+-+-+-+-+-+-+-+-+-+-+-+-+-+-//
+var number = 10;
 var intervalId;
 
 var correctAnswers = 0;
 var incorrectAnswers = 0;
-var unAnswers = 0;
+var unansweredAnswers = 0;
 
-var radioValue;
+var radioValueOne;
+var radioValueTwo;
 
-// Functions
-
-// var radioValue = $("input[name='choice']").val();
+//+-+-+-+-+-+-+-+-+-+-+-+-+-+- Functions +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-//
 
 
+// `:checked` checks if the radio input has been selected
 $("input[type='radio']").click(function() {
-    radioValue = $("input[name=choice]:checked").val();
-    console.log(radioValue);
+    radioValueOne = $("input[name='choice1']:checked").val();
 });
 
+$("input[type='radio']").click(function() {
+    radioValueTwo = $("input[name='choice2']:checked").val();
+});
 
-
-
-
-//=============================== Timer =============================================//
+// Timer
 function runTimer() {
     // Clears timer set. This is placed in the beginning to prevent multiple instances.
     clearInterval(intervalId);
@@ -44,24 +40,58 @@ function timerDown() {
 
     if(number === 0) {
         stopTimer();
-        if(radioValue === "wrongChoice"){
-            incorrectAnswers++;
-            
-            console.log("User submitted: " + radioValue);
-            $("#incorrectAnswers").html(incorrectAnswers);
-        }
-    
-        else if(radioValue === "rightChoice"){
-            correctAnswers++;
-            
-            console.log("User submitted: " + radioValue);
-            $("#correctAnswers").html(correctAnswers);
-            
-        }
+
+        // Attempt to get it to reset
+        // setInterval(function(){ 
+        //     number = 10;
+        //     runTimer();
+        // }, 3000);
+
+    //    for (i = 0; i < 1; i++) {
+            if (radioValueOne === "wrongChoice") {
+                incorrectAnswers++;
+                
+                console.log("User submitted: " + radioValueOne);
+                $("#incorrectAnswers").html(incorrectAnswers);
+            }
+        
+            else if (radioValueOne === "rightChoice") {
+                correctAnswers++;
+                
+                console.log("User submitted: " + radioValueOne);
+                $("#correctAnswers").html(correctAnswers);
+                
+            }
+
+            else // if (radioValueOne === false || radioValueTwo === false) {
+                unansweredAnswers++;
+                $("#unansweredAnswers").html(unansweredAnswers);
+            // }
+
+            if (radioValueTwo === "wrongChoice") {
+                incorrectAnswers++;
+                
+                console.log("User submitted: " + radioValueTwo);
+                $("#incorrectAnswers").html(incorrectAnswers);
+            }
+        
+            else if (radioValueTwo === "rightChoice") {
+                correctAnswers++;
+                
+                console.log("User submitted: " + radioValueTwo);
+                $("#correctAnswers").html(correctAnswers);
+                
+            }
+
+            else // if (radioValueOne === false || radioValueTwo === false) {
+                unansweredAnswers++;
+                $("#unansweredAnswers").html(unansweredAnswers);
+            // }
+    //    }
     }
 }
 
 
 
-// Main Process
+//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+- Main Process +-+-+-+-+-+-+-+-+-+-+-+-+-+-//
 runTimer();
